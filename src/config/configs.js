ea375041c5d9c6310572cdff768b6f75c4f1ffdb
Harshwardhan Deshmukh <harshwardhanet7007@gmail.com>
2025-04-10 02:27:16 +0530

    minor update - configs

--- src/config/configs.js
+++ src/config/configs.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT || 3000
 const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
 const SERVER_TIMESTAMP = new Date();
 const JWT_SECRET = process.env.JWT_SECRET
+const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
 
 module.exports = {
     PORT,
     MONGODB_CONNECTION_URL,
     SERVER_TIMESTAMP,
-    JWT_SECRET
+    JWT_SECRET,
+    SALT_ROUNDS
 }
--- src/utils/passwordHasher.js
+++ src/utils/passwordHasher.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcryptjs");
-
-const SALT_ROUNDS = 10;
+const { SALT_ROUNDS } = require("../config/configs");
 
 function hashPassword(password) {
     return bcrypt.hashSync(password, SALT_ROUNDS);
