608c3e3eb45edfb13fa61a9af95bde96afdd4e75
Harshwardhan Deshmukh <58629753+Harshwardhan-Deshmukh@users.noreply.github.com>
2025-04-10 09:12:19 +0530

    config updated - mongo connection url

--- src/config/configs.js
+++ src/config/configs.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config(); // .env is added in root directory
 
 const PORT = process.env.PORT || 3000
-const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL || "mongodb://localhost:27017/foodchainapp";
 const SERVER_TIMESTAMP = new Date();
 const JWT_SECRET = process.env.JWT_SECRET
 const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
